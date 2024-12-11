c617c10b20e8c64c014a23e0919e8e1440fd2f57
wahyuh1997 <wahyuh1997@gmail.com>
2024-12-11 13:35:02 +0700

    show product list

---
+++ src/Api.jsx
@@ -0,0 +1,5 @@
+export const API_URL = "https://fakestoreapi.com/";
+
+export const headers = {
+  "Content-Type": "application/json",
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { API_URL } from "./api";
+import { Card, Col, Layout, Menu, Row, Spin, theme } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  /* Template Layout ANTD */
+  const { Header, Content, Footer } = Layout;
+  /* Card */
+  const { Meta } = Card;
+
+  const items = new Array(3).fill(null).map((_, index) => ({
+    key: String(index + 1),
+    label: `nav ${index + 1}`,
+  }));
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const response = await fetch(API_URL + "products");
+        if (!response.ok) {
+          throw new Error("Failed Fetch Data");
+        }
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          ></Menu>
+        </Header>
+
+        <Content>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 380,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            {loading && <Spin />}
+
+            <Row gutter={[16, 16]}>
+              {products.map((product) => (
+                <Col key={product.id} span={4} className="gutter-row">
+                  <Card
+                    key={product.id}
+                    hoverable
+                    style={{ width: "100%", textAlign: "center" }}
+                    cover={
+                      <img
+                        alt={product.title}
+                        src={product.image}
+                        style={{ height: "18rem" }}
+                      />
+                    }
+                  >
+                    <Meta
+                      title={product.title}
+                      description={"$" + product.price}
+                    />
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
